f5368b4f89ba01672a6cae1571ba7cfa98b48a51
Simon Vargas <sjvargas01@gmail.com>
2022-10-03 23:35:31 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            
+            @Override
+            public void run() {
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+
+            }
+        });
+
+        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before staring a new thread");
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName()+ " after starting a new thread");
+
+        Thread.sleep(1000);
+
+    }
+}
